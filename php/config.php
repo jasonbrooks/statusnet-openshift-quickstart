f3342b3ee04dcf61d3b13aa1cc81d59de04f8f5a
Jason Brooks <jason@sealrock.net>
2012-08-23 11:35:54 -0700

    add config file

---
+++ php/config.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('STATUSNET') && !defined('LACONICA')) { exit(1); }
+
+$config['site']['name'] = $_ENV['OPENSHIFT_APP_NAME'];
+
+$config['site']['server'] = $_ENV['OPENSHIFT_APP_DNS'];
+$config['site']['path'] = false; 
+
+$config['site']['fancy'] = true;
+
+$config['db']['database'] = $_ENV['OPENSHIFT_DB_URL']\/$_ENV['OPENSHIFT_APP_NAME'];
+
+$config['db']['type'] = $_ENV['OPENSHIFT_DB_TYPE'];
+
+$config['site']['profile'] = 'private';
+
