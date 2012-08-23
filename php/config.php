6a45a27745b09aeaccf6338a9dbe11e1033fd5a2
Jason Brooks <jason@sealrock.net>
2012-08-23 11:42:14 -0700

    config tweak

--- php/config.php
+++ php/config.php
@@ -8,7 +8,7 @@ $config['site']['path'] = false;
 
 $config['site']['fancy'] = true;
 
-$config['db']['database'] = $_ENV['OPENSHIFT_DB_URL']\/$_ENV['OPENSHIFT_APP_NAME'];
+$config['db']['database'] = $_ENV['OPENSHIFT_DB_URL'];
 
 $config['db']['type'] = $_ENV['OPENSHIFT_DB_TYPE'];
 
